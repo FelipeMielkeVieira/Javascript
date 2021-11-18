95f1c2eac9c7d45a66908b77897542ea202035ce
JuPalaczeski <julia_palaczeski@estudante.sc.senai.br>
2021-11-18 20:31:43 -0300

    18/11

--- index.js
+++ index.js
@@ -4,17 +4,95 @@ let tela = document.querySelector("main");
 tela.className = 'main'
 let botao = document.createElement("button");
 let caixa;
-let nm;
-let sobrenome;
-let data;
+
+let nome = document.createElement('input');
+nome.placeholder = 'Nome';
+nome.className = 'nome';
+
+let sobrenome = document.createElement('input');
+sobrenome.placeholder = 'Sobrenome';
+sobrenome.className = 'nome';
+
+let data = document.createElement('input');
+data.type = 'date'
+data.placeholder = 'Data de Nascimento';
+data.className = 'nome';
+
+let botao1 = document.createElement('button');
+botao1.className = 'bt1';
+botao1.innerText = 'Cadastrar';
+botao1.onclick = cadastrar;
+
+let botao2 = document.createElement('button');
+botao2.className = 'bt1';
+botao2.innerText = 'Cancelar';
+botao2.onclick = voltar;
+
+let lista = [];
+
+let mensagem = document.createElement("div");
+
 botao.className = 'botao';
 botao.innerText = 'Cadastre-se'
 botao.onclick = modal;
 telatotal.appendChild(botao);
 
+function alert(a) {
+
+    if(a == 1) {
+
+        mensagem.className = 'mensagem';
+        mensagem.style.borderColor = 'rgb(141, 15, 15)'
+        mensagem.style.backgroundColor = 'red';
+        mensagem.innerText = 'Preencha todos os campos!';
+        telatotal.appendChild(mensagem);
+
+    } else {
+
+        mensagem.className = 'mensagem';
+        mensagem.style.borderColor = 'green';
+        mensagem.style.backgroundColor = 'rgb(20, 161, 20)';
+        mensagem.innerText = 'Cadastro realizado com sucesso!';
+        telatotal.appendChild(mensagem);
+        telatotal.removeChild(caixa);
+        nome.value = '';
+        sobrenome.value = '';
+        data.value = '';
+
+    }
+
+    setTimeout(tirarmensagem, 3500);
+
+}
+
+let tabela = document.createElement("div");
+tabela.className = 'tabela';
+
+let linha = document.createElement("div");
+linha.className = 'linha';
+
+let dado = document.createElement("div");
+dado.className = 'dado';
+
+// function tabela(){
+
+//     telatotal.appendChild(tabela);
+//     tabela.appendChild(linha);
+
+//     linha.appendChild(dado);
+    
+// }
+
+function tirarmensagem() {
+    telatotal.removeChild(mensagem);
+}
+
 function voltar() {
     telatotal.removeChild(caixa);
     tela.style.opacity = '1';
+    nome.value = '';
+    sobrenome.value = '';
+    data.value = '';
 }
 
 function modal() {
@@ -27,31 +105,13 @@ function modal() {
     titulo.innerText = 'Cadastre-se';
     titulo.className = 'tit';
     caixa.appendChild(titulo);
-    nm = document.createElement('input');
-    nm.placeholder = 'Nome';
-    nm.className = 'nome';
-    sobrenome = document.createElement('input');
-    sobrenome.placeholder = 'Sobrenome';
-    sobrenome.className = 'nome';
-    data = document.createElement('input');
-    data.type = 'date'
-    data.placeholder = 'Data de Nascimento';
-    data.className = 'nome';
     let info = document.createElement('span');
     info.innerText = 'Ao criar uma conta, você concorda com as Condições de Uso.'
     info.className = 'info'
     let inferior = document.createElement('div');
     inferior.className = 'inferior';
-    let botao1 = document.createElement('button');
-    botao1.className = 'bt1';
-    botao1.innerText = 'Cadastrar';
-    botao1.onclick = cadastrar;
-    let botao2 = document.createElement('button');
-    botao2.className = 'bt1';
-    botao2.innerText = 'Cancelar';
-    botao2.onclick = voltar;
     caixa.appendChild(caixap);
-    caixap.appendChild(nm);
+    caixap.appendChild(nome);
     caixap.appendChild(sobrenome);
     caixap.appendChild(data);
     caixap.appendChild(info);
@@ -62,15 +122,30 @@ function modal() {
 }
 
 function cadastrar() {
-    let variavel = {
-        a: nm.value,
-        b: sobrenome.value,
-        c: data.value
+
+    let nome2 = nome.value;
+    let sobre = sobrenome.value;
+    let dat = data.value;
+
+    if(nome2 != '' && sobre != '' && dat != '') {
+
+        let variavel = {
+            a: nome2,
+            b: sobre,
+            c: dat
+        }
+    
+        lista.push(variavel);
+    
+        console.log(variavel);
+        console.log(lista);
+
+        alert(2);
+
+    } else {
+
+        alert(1);
+
     }
 
-    let lista = [];
-
-    lista.push(variavel);
-
-    return lista;
 }
